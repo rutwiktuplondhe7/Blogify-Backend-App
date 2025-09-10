56f56a51c419321c5abd570d7363b58295549645
Tuplondhe Rutwik <rutwiktuplondhe7@gmail.com>
2025-09-10 15:06:56 +0530

    final push

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const app = express();
+const PORT = 8000;
+
+const Blog = require('./models/blog')
+
+mongoose.connect('mongodb://localhost:27017/blogify').then(e => console.log('MongoDB Connected'));
+
+const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
+const { checkForAuthenticationCookie } = require('./middleware/authentication');
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve('./views'));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')))
+
+app.get('/', async (req, res) => {
+    const allBlogs = await Blog.find({})
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server started at PORT: ${PORT}`);
+});
---
+++ middleware/authentication.js
@@ -0,0 +1,21 @@
+const { validateToken } = require("../service/authentication");
+
+function checkForAuthenticationCookie(cookieName) {
+    return (req, res, next) => {
+        const tokenCookieValue = req.cookies[cookieName];
+        if(!tokenCookieValue) {
+            return next();
+        }
+        try {
+            const userPayLoad = validateToken(tokenCookieValue);
+            req.user = userPayLoad;
+        } catch (error) {
+
+        }
+        return next();
+    }
+}
+
+module.exports = {
+    checkForAuthenticationCookie,
+};
